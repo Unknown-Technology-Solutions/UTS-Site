38202e29ac1a69cf6035dc6af490316e10d40c7d
Zane Reick <zane.reick@unknownts.tk>
2020-11-05 11:20:45 -0600

    Made it easier to change database version

--- modern_database.sql
+++ modern_database.sql
@@ -1,13 +1,14 @@
-CREATE DATABASE uts_modern;
-GRANT ALL privileges ON `uts_modern`.* TO 'modern'@localhost;
+SET @DB_VERSION = "uts_modern_v1";
+CREATE DATABASE IF NOT EXISTS @DB_VERSION;
+GRANT ALL privileges ON @DB_VERSION.* TO 'modern'@localhost;
 FLUSH PRIVILEGES;
-use uts_modern;
+use @DB_VERSION;
 CREATE TABLE `customer_requests` (
 	`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`first_name` VARCHAR(50) NULL DEFAULT NULL,
 	`last_name` VARCHAR(50) NULL DEFAULT NULL,
 	`company` VARCHAR(50) NULL DEFAULT NULL,
-	`request` VARCHAR(50) NULL DEFAULT NULL,
+	`request` VARCHAR(2048) NULL DEFAULT NULL,
 	`email` VARCHAR(50) NULL DEFAULT NULL,
 	`submit_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
