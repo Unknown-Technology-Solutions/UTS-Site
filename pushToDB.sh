beb104b0ad0b46d0257b8679c4bfa179d1a51a5b
Zane Reick <zane.reick@unknownts.tk>
2020-11-06 16:18:40 -0600

    Removed debug echos, changed log location

--- pushToDB.sh
+++ pushToDB.sh
@@ -3,10 +3,8 @@
 WORKING_DIR=/home/buildbot/worker/runtests/build/
 
 echo "Reseting UTS Testing databases"
-echo $MSQL_UNAME
-echo $MSQL_PASS
-mysql --user=$MSQL_UNAME --password=$MSQL_PASS < $WORKING_DIR/modern_database.sql > /var/www/testing/output.tab
-cat /var/www/testing/output.tab
+mysql --user=$MSQL_UNAME --password=$MSQL_PASS < $WORKING_DIR/modern_database.sql > $HOME/output.tab
+cat $HOME/output.tab
 
 echo "Finished!"
 exit 0
