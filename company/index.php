c737397c2a4fe61389e3862a02e09badf8c17c41
Zane Reick <zane.reick@unknownts.tk>
2020-11-18 11:33:30 -0600

    Linking stuff & adding stuff

--- company/index.php
+++ company/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Company Login</title>
         <script defer src="/script-resources/menu.js"></script>
         <script defer src="/script-resources/clock.js"></script>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <link rel="stylesheet" type="text/css" href="/css/modern.css" />
         <link rel="stylesheet" type="text/css" href="/css/blinking_cursor.css" />
         <link rel="stylesheet" type="text/css" href="/css/menu.css" />
@@ -16,11 +16,13 @@
                 font-size: 22px;
             }
         </style>
+        <title>Company Login</title>
     </head>
     <body onload="startTime()">
         <noscript>Sorry, your browser does not support JavaScript!</noscript>
         <div id="mySidenav" class="sidenav">
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="/index.html">UTS Homepage</a>
             <br />
             <a href="#"><span id="clock"></span></a>
         </div>
