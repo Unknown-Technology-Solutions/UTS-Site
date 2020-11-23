66e7406de94b0970babf932c7d8bc312a9a9402b
Zane Reick <zane.reick@unknownts.tk>
2020-11-22 23:36:51 -0600

    cont'd work on a login API

--- api/login.php
+++ api/login.php
@@ -1,1 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css" href="/css/modern.css" />
+        <title>Login API</title>
+    </head>
+</html>
 <?php
+if (!isset($_POST['submit'])) {
+    die();
+}
