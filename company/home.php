b573d6563e88907b29089a01ec43495f889773d4
Zane Reick <zane.reick@unknownts.tk>
2020-11-24 21:34:10 -0600

    The test page hurt my eyes, so i made it darkmode

--- company/home.php
+++ company/home.php
@@ -1,2 +1,7 @@
-<?php
-echo("Temp");
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css" href="/css/modern.css" />
+        <title>Home</title>
+    </head>
+</html>
