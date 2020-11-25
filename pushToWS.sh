388acf0cf78b2e92cbead32101ec347aea92fbda
Zane Reick <zane.reick@unknownts.tk>
2020-11-24 21:49:18 -0600

    Added API to testing deploy script

--- pushToWS.sh
+++ pushToWS.sh
@@ -13,11 +13,12 @@ cp $WORKING_DIR/*.html    /var/www/testing
 cp $WORKING_DIR/*.php     /var/www/testing
 cp $WORKING_DIR/.htaccess /var/www/testing
 echo "Moving folders to Testing"
-#cp -R $WORKING_DIR/data /var/www/testing
+#cp -R $WORKING_DIR/ /var/www/testing
 cp -R $WORKING_DIR/script-resources/ /var/www/testing
 cp -R $WORKING_DIR/company/          /var/www/testing
-cp -R $WORKING_DIR/css            /var/www/testing
-cp -R $WORKING_DIR/downloads      /var/www/testing
+cp -R $WORKING_DIR/css               /var/www/testing
+cp -R $WORKING_DIR/downloads         /var/www/testing
+cp -R $WORKING_DIR/api               /var/www/testing
 
 echo "Re-owning files"
 #touch /var/www/testing/output.tab
