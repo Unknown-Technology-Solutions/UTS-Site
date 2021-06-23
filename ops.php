590616116e29a18ac24b85b14ff51172701d3da7
Zane Reick <zane.reick@unknownts.tk>
2021-06-23 15:15:42 -0500

    flow correction

--- ops.php
+++ ops.php
@@ -40,7 +40,7 @@
     <div class="title">Unknown<br />Technology<br />Solutions<span class="blinking-cursor" style="font-size: 1em;">|</span></div>
     <div class="intro heading" id="1">The Panopticon</div>
     <div class="intro">
-        When faced with tomorrow's security challenges, today, we decided we needed a new approach to security; enter The&nbsp;Panopticon.<br />
+        When faced with tomorrow's security challenges today, we decided we needed a new approach to security; enter The&nbsp;Panopticon.<br />
         The&nbsp;Panopticon is our network and security center, physical and digital.
         Here we monitor all network traffic and security systems. With The&nbsp;Panopticon active, we are not only able to respond to threats that are active, but threats that are developing.
         By analyzing traffic we are able to spot trends and attacks before they even happen saving time and money.<br />
