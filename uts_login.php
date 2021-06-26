21594c4bea3bd3a32ea05eef293a22e64c99a85c
Zane Reick <zane.reick@unknownts.tk>
2021-06-26 14:44:58 -0500

    Fixed a minor issue

--- uts_login.php
+++ uts_login.php
@@ -1,5 +1,5 @@
 <?php
-include_once('../functions.php');
+include_once('./functions.php');
 $authState = authenticateAgainstEmployee();
 if ($authState[0] == true) {
     header("/company/home.php");
