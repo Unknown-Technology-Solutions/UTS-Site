60e5b2a4d17ca2a6f81dd229c7f49c934bee0f1e
boorishpea <mrchristianriley@gmail.com>
2021-07-13 22:03:31 +0100

    Update email_registration.php
    added optional json output for results

--- email_registration.php
+++ email_registration.php
@@ -1,3 +1,70 @@
+<?php
+//Connection info for the remote database
+
+include_once('./functions.php');
+$connect_r = mail_db();
+
+$GET_USER_IP = $_SERVER['REMOTE_ADDR'];
+
+if ($connect_r->connect_error) {
+	header("Content-Type: text/plain");
+	die("Connection failed: " . $connect_r->connect_error);
+}
+
+function json_result($error, $error_message = "")
+{
+	header("Content-Type: text/plain");
+	print(json_encode(array("error" => $error, "error_message" => $error_message)));
+	die();
+}
+
+$success = false;
+$error = false;
+$error_message = "no error";
+
+if (isset($_POST['submit'])) {
+	$ip = $GET_USER_IP;
+	$m_username = $connect_r->real_escape_string($_POST['m_username']);
+	$m_password = $connect_r->real_escape_string($_POST['m_password']);
+	$n_username = $connect_r->real_escape_string($_POST['n_username']);
+	$n_password = $connect_r->real_escape_string($_POST['n_password']);
+	$exploded = explode('@', $n_username);
+	$domain = end($exploded);
+
+	$domain_sql =  "SELECT * FROM virtual_domains WHERE name='".$domain."';";
+	$domain_id_sql =  "SELECT id FROM virtual_domains WHERE name='".$domain."';";
+	$domain_id = $connect_r->query($domain_id_sql);
+	
+	$domain_res = $connect_r->query($domain_sql);
+
+	if (mysqli_num_rows($domain_res) == 0) {
+		$error = true;
+		$error_message = "Invalid email! Check that the domain name is valid! (The domain you used: " . strval($domain) . ")";
+		if(isset($_GET['json'])) {
+			json_result($error, $error_message);
+		}
+	} else {
+		print("Creating account...");
+		$submit_sql =  "INSERT INTO virtual_users (domain_id, password, email, ip) VALUES (".$domain_id['id'].", ENCRYPT('" . $n_password . "', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), '" . $n_username . "', '" . $ip . "');";
+		$output = $connect->query($submit_sql);
+		//print(strval($output));
+		if (strval($output) == strval(1)) {
+			$success = true;
+			$message = "Account successfully registered! (" . $username . ")";
+			if(isset($_GET['json'])) {
+				json_result(false);
+			}
+		} else {
+			$error = true;
+			$error_message = "Account failed to register. Try again, or contact an administrator.";
+			if(isset($_GET['json'])) {
+				json_result($error, $error_message);
+			}
+		}
+	}
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,18 +89,6 @@
 </head>
 
 <body>
-    <?php
-    //Connection info for the remote database
-
-    include_once('./functions.php');
-    $connect_r = mail_db();
-
-    $GET_USER_IP = $_SERVER['REMOTE_ADDR'];
-
-    if ($connect_r->connect_error) {
-        die("Connection failed: " . $connect_r->connect_error);
-    }
-    ?>
     <div id="main">
         <div class="title">Register Email Account<span class="blinking-cursor" style="font-size: 1em;">|</span></div>
         <div class="intro heading">
@@ -49,36 +104,13 @@
             </form>
         </div>
         <?php
-        //$connect->real_escape_string
-        if (isset($_POST['submit'])) {
-            $ip = $GET_USER_IP;
-            $m_username = $connect_r->real_escape_string($_POST['m_username']);
-            $m_password = $connect_r->real_escape_string($_POST['m_password']);
-            $n_username = $connect_r->real_escape_string($_POST['n_username']);
-            $n_password = $connect_r->real_escape_string($_POST['n_password']);
-            $exploded = explode('@', $n_username);
-            $domain = end($exploded);
-
-            $domain_sql =  "SELECT * FROM virtual_domains WHERE name='".$domain."';";
-            $domain_id_sql =  "SELECT id FROM virtual_domains WHERE name='".$domain."';";
-            $domain_id = $connect_r->query($domain_id_sql);
-            
-            $domain_res = $connect_r->query($domain_sql);
-
-            if (mysqli_num_rows($domain_res) == 0) {
-                print("Invalid email! Check that the domain name is valid! (The domain you used: " . strval($domain) . ")");
-            } else {
-                print("Creating account...");
-                $submit_sql =  "INSERT INTO virtual_users (domain_id, password, email, ip) VALUES (".$domain_id['id'].", ENCRYPT('" . $password . "', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), '" . $username . "', '" . $ip . "');";
-                $output = mysqli_query($connect, $submit_sql);
-                print(strval($output));
-                if (strval($output) == strval(1)) {
-                    print("Account successfully registered! (" . $username . ")");
-                } else {
-                    print("Account failed to register. Try again, or contact an administrator.");
-                }
-            }
-        }
+        if ($error) {
+			print($error_message);
+		}
+		else if($success) {
+			print("Creating account...");
+			print("Account successfully registered! (" . strip_tags($_POST['n_username']) . ")");
+		}
         ?>
         <footer>
             Unknown Technology Solutions 2017-<?php echo date('Y'); ?><br />
