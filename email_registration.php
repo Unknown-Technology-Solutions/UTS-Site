cee7aa837a8f224689da9872430c4a610497af3e
Zane Reick <zane.reick@unknownts.tk>
2021-07-13 17:30:45 -0500

    try normal things

--- email_registration.php
+++ email_registration.php
@@ -41,7 +41,7 @@ if (isset($_POST['submit'])) {
 		$GLOBALS['error'] = true;
 		$GLOBALS['message'] = "Invalid email! Check that the domain name is valid! (The domain you used: " . strval($domain) . ")";
 	} else {
-		$submit_sql =  "INSERT INTO `mailserver`.`virtual_users` (domain_id, password, email, ip) VALUES (".$di['id'].", ENCRYPT('" . $n_password . "', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), '" . $n_username . "', '" . $ip . "');";
+		$submit_sql =  "INSERT INTO virtual_users (domain_id, password, email, ip) VALUES (".$di['id'].", ENCRYPT('" . $n_password . "', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), '" . $n_username . "', '" . $ip . "');";
 		$output = $connect->query($submit_sql);
 		if (strval($output) == strval(1)) {
 			$GLOBALS['success'] = true;
