59aff8c0cc85797062ff17a67b0ff7144d058fff
Zane Reick <zane.reick@unknownts.tk>
2021-07-26 10:12:24 -0500

    Updated mailbox API

--- api/functions.php
+++ api/functions.php
@@ -1,7 +1,30 @@
 <?php
-//Create a protect function to keep the database safe
 function protect($string)
 {
     $string = trim(strip_tags(addslashes($string)));
     return $string;
+}
+
+$web_settings = parse_ini_file("../web_settings.ini.php");
+
+function mail_db()
+{
+    global $web_settings;
+    $rservername = $web_settings['r_ip'];
+    $rusername = $web_settings['r_username'];
+    $rpassword = $web_settings['r_password'];
+    $rdatabase = $web_settings['r_database'];
+    $rport = $web_settings['r_port'];
+    $connect_r = new mysqli($rservername, $rusername, $rpassword, $rdatabase, $rport);
+    return $connect_r;
+}
+
+
+function handlePassword($p, $q, $h_p = null)
+{
+    if ($q == "hash") {
+        return password_hash($p, PASSWORD_BCRYPT);
+    } elseif ($q == "verify") {
+        return password_verify($p, $h_p);
+    }
 }
--- api/mailbox_stats.php
+++ api/mailbox_stats.php
@@ -1,9 +1,39 @@
 <?php
+/*
+* POST Headers:
+* submit   = (anything)
+* username = (mailbox username)
+* password = (mailbox password)
+* request  = (request type)
+*/
+
 
 header("Content-Type: application/json");
 
 
-$mbox = imap_open("{localhost:993/imap/ssl}INBOX", "user_id", "password");
+if (isset($_POST['submit'])) {
+    $connect_r = mail_db();
+    $username = $connect_r->real_escape_string($_POST['username']);
+    $password = $connect_r->real_escape_string($_POST['password']);
+
+    $username_in_db =  "SELECT username FROM virtual_users WHERE username='" . $username . "';";
+    $un_q = $connect_r->query($username_in_db);
+    if (mysqli_num_rows($un_q) == 0) {
+        //TODO: FAILED
+    } else {
+        $password_in_db =  "SELECT username,password FROM virtual_users WHERE username='" . $username . "' AND password='" . $password . "';";
+        $pw_q = $connect_r->query($username_in_db);
+        if (mysqli_num_rows($pw_q) == 0) {
+            //TODO: FAILED
+        } else {
+            $pass = $pw_q->fetch_assoc();
+            
+        }
+    }
+}
+
+
+//$mbox = imap_open("{localhost:993/imap/ssl}INBOX", "user_id", "password");
 
 
 die(json_encode(array("error" => false, "message" => "bleh")));
