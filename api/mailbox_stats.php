a6bdba893bb691f73100d18872091534b06c5ed2
Zane Reick <zane.reick@unknownts.tk>
2021-07-28 14:02:29 -0500

    Inbox count is now supported and the default action

--- api/mailbox_stats.php
+++ api/mailbox_stats.php
@@ -50,15 +50,25 @@ if (isset($_POST['submit'])) {
 if (isset($_POST['submit']) && $GLOBALS['auth']) {
     //TODO: Mailbox handling
     $mailbox = new PhpImap\Mailbox(
-        '{imap.unknownts.com:993/ssl/novalidate-cert/imap}',
+        '{imap.unknownts.com:993/ssl/novalidate-cert/imap}INBOX',
         $username,
         $password,
         false // dont save attachments
     );
     $GLOBALS['return_array'] = ["mailbox_access" => true];
 
+    // Read mail in mailbox since before mailserver existed to ensure all mail is seen
+    $mailsIds = $mailbox->searchMailbox('SINCE "20170101"');
+    if (!$mailsIds) {
+        //TODO: Handle empty mailbox
+    }
 
-    
+    $tmp_arr = ["inbox_count" => count($mailsIds)];
+    $GLOBALS['return_array'] = array_merge($GLOBALS['return_array'], $tmp_arr);
+
+    // Show total number of emails
+    //echo 'n= ' . count($mailsIds) . '<br>';
+
     print(jsonErrorOut(ErrorCode::Success));
 }
 
