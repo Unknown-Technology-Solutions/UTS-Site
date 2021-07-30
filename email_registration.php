38a993e8f199ca9b2d1029145819a80a8855e10b
Zane Reick <zane.reick@unknownts.tk>
2021-07-30 00:25:20 -0500

    Re-wrote the email registration to make it better

--- email_registration.php
+++ email_registration.php
@@ -1,112 +1,78 @@
 <?php
-
-//Connection info for the remote database
-
+// Include needed files
 include_once('./functions.php');
-$connect_r = mail_db();
+include_once('./errors.php');
+include_once('./authentication.php');
 
-$GET_USER_IP = $_SERVER['REMOTE_ADDR'];
+// Define variables
+$USER_IP = $_SERVER['REMOTE_ADDR'];
+$connect_r = mail_db(); // Remote database connection
 
-abstract class CreateAccountResult
-{
-	const Skip = 0;
-    const Fail = 1;
-    const Error = 1;
-	const Success = 2;
-}
-
-
-abstract class CreateAccountErrorCode {
-    const Success = 200;
-    const Login   = 401;
-    const Invalid = 400;
-    const Failure = 499;
-    const Bad     = 403;
-}
 
 if ($connect_r->connect_error) {
-	header("Content-Type: application/json");
-	//print(json_encode(array("error" => true, "message" => "There has been an internal error.")));
-	//die("Connection failed: " . $connect_r->connect_error);
-	//die();
-	die(json_encode(array("error" => CreateAccountResult::Fail, "errorcode" => CreateAccountErrorCode::Failure, "message" => "Contact support@unknownts.com")));
+    header("Content-Type: application/json");
+    die(jsonErrorOut(ErrorCode::InternErr));
 }
 
+if (isset($_POST['submit']) || isset($_POST['submit_json'])) { //we have to be backwards compatable
 
+    $m_username = $_POST['m_username'];
+    $m_password = $_POST['m_password'];    
+    $n_username = $connect_r->real_escape_string($_POST['n_username']);
+    $n_password = $connect_r->real_escape_string($_POST['n_password']);
+    $exploded = explode('@', $n_username);
+    $domain = end($exploded);
 
-$GLOBALS['result'] = CreateAccountResult::Skip;
-$GLOBALS['message'] = "no error";
+    $domain_sql =  "SELECT id, name FROM virtual_domains WHERE name='" . $domain . "';";
+    $domain_info = $connect_r->query($domain_sql);
+    $di = $domain_info->fetch_assoc();
 
-// Handle submit
-
-if (isset($_POST['submit'])||isset($_POST['submit_json'])) {
-
-	$ip = $GET_USER_IP;
-	$m_username = $connect_r->real_escape_string($_POST['m_username']);
-	$m_password = $connect_r->real_escape_string($_POST['m_password']);
-	$n_username = $connect_r->real_escape_string($_POST['n_username']);
-	$n_password = $connect_r->real_escape_string($_POST['n_password']);
-	$exploded = explode('@', $n_username);
-	$domain = end($exploded);
-
-	$domain_sql =  "SELECT id, name FROM virtual_domains WHERE name='" . $domain . "';";
-	$domain_info = $connect_r->query($domain_sql);
-	$di = $domain_info->fetch_assoc();
-
-	if (mysqli_num_rows($domain_info) == 0) {
-        $GLOBALS['result'] = CreateAccountResult::Error;
-        $GLOBALS['errorcode'] = CreateAccountErrorCode::Invalid;
-		$GLOBALS['message'] = "Invalid email! Check that the domain name is valid! (The domain you used: " . strval($domain) . ")";
-	} else {
-		if (!$m_username || !$m_password) {
-            $GLOBALS['result'] = CreateAccountResult::Error;
-            $GLOBALS['errorcode'] = CreateAccountErrorCode::Login;
-			$GLOBALS['message'] = "Username and password required";
-		} else {
-			$m_req = "SELECT email,password,new_user_authorized,authorized_domains FROM virtual_users WHERE email='" . $m_username . "';";
-			$m_res = $connect_r->query($m_req);
-			$m_asc = $m_res->fetch_assoc();
-			if (mysqli_num_rows($m_res) == 0) {
-                $GLOBALS['result'] = CreateAccountResult::Error;
-                $GLOBALS['errorcode'] = CreateAccountErrorCode::Login;
-				$GLOBALS['message'] = "Bad username or password";
-			} else {
-				if (handlePassword($m_password, "verify", $m_asc['password'])) {
-					if ($m_asc['new_user_authorized'] == "true" && isAuthorizedForDomain($di['id'], $m_asc['authorized_domains'])) {
-						$submit_sql =  "INSERT INTO virtual_users (domain_id, password, email, ip, master) VALUES (" . $di['id'] . ", '" . handlePassword($n_password, "hash") . "', '" . $n_username . "', '" . $ip . "', '" . $m_username . "');";
-						$output = $connect_r->query($submit_sql);
-						//$output = 0; //temporary lockout
-						if (strval($output) == strval(1)) {
-                            $GLOBALS['result'] = CreateAccountResult::Success;
-                            $GLOBALS['errorcode'] = CreateAccountErrorCode::Success;
-							$GLOBALS['message'] = "Account successfully registered! (" . strip_tags($n_username) . ")";
-						} else {
-                            $GLOBALS['result'] = CreateAccountResult::Error;
-                            $GLOBALS['errorcode'] = CreateAccountErrorCode::Failure;
-							$GLOBALS['message'] = "Account failed to register. Try again, or contact an administrator. Error(s): " . strval($connect_r->error);
-							//$GLOBALS['message'] = strval($connect_r->error);
-						}
-					} else {
-                        $GLOBALS['result'] = CreateAccountResult::Error;
-                        $GLOBALS['errorcode'] = CreateAccountErrorCode::Bad;
-						$GLOBALS['message'] = "Bad permissions";
-					}
-				} else {
-                    $GLOBALS['result'] = CreateAccountResult::Error;
-                    $GLOBALS['errorcode'] = CreateAccountErrorCode::Login;
-					$GLOBALS['message'] = "Bad username or password";
-				}
-			}
-		}
-	}
-	// Respond with JSON if required
-	if(isset($_GET['json'])||isset($_POST['json'])||isset($_POST['submit_json'])) {
-		header("Content-Type: application/json");
-		die(json_encode(array("error" => $GLOBALS['result'], "errorcode" => $GLOBALS['errorcode'], "message" => $GLOBALS['message'])));
-	}
+    if (mysqli_num_rows($domain_info) == 0) {
+        $GLOBALS['message'] = "Invalid email! Check that the domain name is valid! (The domain you used: " . strval($domain) . ")";
+        $GLOBALS['ErrorCode'] = ErrorCode::InvalidReq;
+        //die(jsonErrorOut(ErrorCode::InvalidReq));
+    } else {
+        if (!$m_username || !$m_password) {
+            $GLOBALS['message'] = "Username and password required";
+            $GLOBALS['ErrorCode'] = ErrorCode::InvalidReq;
+            //die(jsonErrorOut(ErrorCode::InvalidReq));
+        } else {
+            $AuthReturnArr = authenticateToMaster($connect_r, $m_username, $m_password);
+            if ($AuthReturnArr['authenticated'] == false) {
+                $GLOBALS['message'] = "Bad username or password";
+                $GLOBALS['ErrorCode'] = $AuthReturnArr['ErrorCode'];
+                //die(jsonErrorOut($AuthReturnArr['ErrorCode']));
+            } else {
+                if ($AuthReturnArr['new_user_authorized'] == "true" && isAuthorizedForDomain($di['id'], $AuthReturnArr['authorized_domains'])) {
+                    $submit_sql =  "INSERT INTO virtual_users (domain_id, password, email, ip, master) VALUES (" . $di['id'] . ", '" . handlePassword($n_password, "hash") . "', '" . $n_username . "', '" . $USER_IP . "', '" . $connect_r->real_escape_string($m_username) . "');";
+                    $output = $connect_r->query($submit_sql);
+                    //$output = 0; //temporary lockout
+                    if (strval($output) == strval(1)) {
+                        $GLOBALS['message'] = "Account successfully registered! (" . strip_tags($n_username) . ")";
+                        $GLOBALS['ErrorCode'] = ErrorCode::Success;
+                        //die(jsonErrorOut(ErrorCode::Success));
+                    } else {
+                        $GLOBALS['message'] = "Account failed to register. Try again, or contact an administrator. Error(s): " . strval($connect_r->error);
+                        $GLOBALS['ErrorCode'] = ErrorCode::InternErr;
+                        //$GLOBALS['message'] = strval($connect_r->error);
+                        //die(jsonErrorOut(ErrorCode::InternErr));
+                    }
+                } else {
+                    $GLOBALS['message'] = "Bad permissions";
+                    $GLOBALS['ErrorCode'] = ErrorCode::NotAllowed;
+                    //die(jsonErrorOut(ErrorCode::NotAllowed));
+                }
+            }
+        }
+    }
+    // Respond with JSON if required
+    if (isset($_GET['json']) || isset($_POST['json']) || isset($_POST['submit_json']) || isset($_POST['submit'])) {
+        header("Content-Type: application/json");
+        die(json_encode(array_merge(OnlyErrorOut($GLOBALS['ErrorCode']), array("message" => $GLOBALS['message']))));
+    }
 }
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -143,12 +109,12 @@ if (isset($_POST['submit'])||isset($_POST['submit_json'])) {
 				<div class="label">New password: </div><input class="tbox" type="password" required name="n_password" style="width: 130px;" autocomplete="off"></input><br />
 				<br />
 				<button type="submit" name="submit">Submit</button>
-                <button type="submit" name="submit_json">Submit & JSON</button>
+                <!--<button type="submit" name="submit_json">Submit & JSON</button>-->
 			</form>
 		</div>
 		<?php
-		if($GLOBALS['result'] != CreateAccountResult::Skip) {
-		  if($GLOBALS['result'] == CreateAccountResult::Success)
+		if($GLOBALS['ErrorCode'] != ErrorCode::Skip) {
+		  if($GLOBALS['ErrorCode'] == ErrorCode::Success)
 			print("Creating account...");
 		  print($GLOBALS['message']);
 		}
