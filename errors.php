a742c25f9867aceca1a9cd18e45f2b507d9ff246
Zane Reick <zane.reick@unknownts.tk>
2021-07-30 00:24:56 -0500

    Added a new error code

--- errors.php
+++ errors.php
@@ -8,6 +8,7 @@ abstract class ErrorCode
     const IncmpltErr = 497;
     const DebugErr   = 498;
     const InternErr  = 499;
+    const Skip       = 999;
 }
 
 function ec2text($ErrorCode)
@@ -19,9 +20,24 @@ function ec2text($ErrorCode)
         403 => "Request not allowed",
         497 => "Incomplete request data",
         498 => "Debugging trigger",
-        499 => "Server side error"
+        499 => "Server side error",
+        999 => "Request skipped"
     ];
     return $ect[$ErrorCode];
+}
+
+function OnlyErrorOut($ec)
+{
+    $ErrorText = ec2text($ec);
+    $AssembleDict = ['ErrorCode' => $ec, 'ErrorMessage' => $ErrorText];
+    return $AssembleDict;
+}
+
+function arrayErrorOut($ec)
+{
+    $ErrorText = ec2text($ec);
+    $AssembleDict = ['ErrorCode' => $ec, 'ErrorMessage' => $ErrorText, 'return' => $GLOBALS['return_array']];
+    return $AssembleDict;
 }
 
 function jsonErrorOut($ec)
