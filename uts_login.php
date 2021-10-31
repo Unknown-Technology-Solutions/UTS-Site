18a27be93247b0f552787b10313b9b7f55a49fc7
boorishpea <mrchristianriley@gmail.com>
2021-10-31 21:33:46 +0000

    Update uts_login.php

--- uts_login.php
+++ uts_login.php
@@ -25,7 +25,7 @@ if (isset($_POST['submit'])) {
 } elseif (isset($_POST['logout'])) {
     logout("auth_token");
 }
-print_r($authState);
+//print_r($authState);
 $state = checkSessionValid("login");
 //error_log((string) $state);
 if($state == null){
