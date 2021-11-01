a405dd590db1fc58e64594acc8c3a8073d7941d5
Zane D. R <zane.reick@unknownts.com>
2021-11-01 10:17:01 -0500

    Push smarty files to TWS
    Should fix the 500 error on the testing server

--- pushToWS.sh
+++ pushToWS.sh
@@ -16,6 +16,7 @@ cp $WORKING_DIR/*.json     /var/www/testing
 cp $WORKING_DIR/.htaccess /var/www/testing
 echo "Moving folders to Testing"
 #cp -R $WORKING_DIR/ /var/www/testing
+cp -R $WORKING_DIR/include           /var/www/testing
 cp -R $WORKING_DIR/script-resources/ /var/www/testing
 cp -R $WORKING_DIR/company/          /var/www/testing
 cp -R $WORKING_DIR/css               /var/www/testing
