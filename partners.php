27d8963bfbb219e91cfdae2409112600d0cd5999
Zane D. R <zane.reick@unknownts.tk>
2022-01-31 14:32:27 -0600

    Some final changes for the partners page

--- partners.php
+++ partners.php
@@ -11,31 +11,26 @@ $smarty->display('header.tpl');
 <div class="grid-row">
     <div class="grid-column">
         <img src="images\IWG_Logo-01-med-s2.png" alt="Infinite Wizardy Games logo"> <br />
-        Infinite Wizardy Games
     </div>
     <div class="grid-column">
         <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png" alt="Google"> <br />
-        Google
+
     </div>
     <div class="grid-column">
         <img src="https://apex.oracle.com/assets/media/company-logos/oracle-white.png?v=1" alt="Oracle"> <br />
-        Oracle
     </div>
 </div>
 <div class="grid-row">
     <div class="grid-column">
         <img src="https://apex.oracle.com/assets/media/company-logos/oracle-white.png?v=1" alt="Oracle"> <br />
-        Oracle
     </div>
 
     <div class="grid-column">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png" alt=""> <br />
-        Amazon
     </div>
 
     <div class="grid-column">
         <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png" alt="Google"> <br />
-        Google
     </div>
 </div>
 <br />
